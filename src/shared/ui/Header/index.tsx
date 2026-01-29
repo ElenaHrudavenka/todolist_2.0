5d8492407cf56c1877af9861c9752c13af48eaa8
ElenaHrudavenka <kisseli@mail.ru>
2026-01-29 12:16:48 +0100

    Changed structure and styles

---
+++ src/shared/ui/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from '@/shared/ui/Header/index.module.scss';
+
+const Header = () => {
+    return (
+        <div className={styles.headerContainer}>
+            <h1>ToDo Lists</h1>
+        </div>
+    )
+};
+
+export { Header };
